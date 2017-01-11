db3eee2da2b9edefe29000777fa2e2760ec2c85d
Francesco Lodolo [:flod] <flod@lodolo.net>
2017-01-10 19:16:52 -0800

    Fix api-errors.log in stores_l10n

--- local_configs/set_configs.sh
+++ local_configs/set_configs.sh
@@ -6,10 +6,10 @@ cp ~/mozilla/git/scripts/local_configs/langchecker.php ~/mozilla/git/langchecker
 
 # Stores l10n
 chmod 777 ~/mozilla/git/stores_l10n/cache
+touch ~/mozilla/git/stores_l10n/logs/api-errors.log
+chmod 777 ~/mozilla/git/stores_l10n/logs/api-errors.log
 cp ~/mozilla/git/scripts/local_configs/stores.php ~/mozilla/git/stores_l10n/app/config/config.inc.php
 
 # Webdashboard
 chmod 777 ~/mozilla/git/webdashboard/cache
 cp ~/mozilla/git/scripts/local_configs/webdashboard.php ~/mozilla/git/webdashboard/app/config/settings.inc.php
-
-
