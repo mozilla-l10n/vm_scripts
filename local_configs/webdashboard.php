80835c559f0cefe2be4e6e709b980fcacca7f7ea
Francesco Lodolo [:flod] <flod@lodolo.net>
2017-07-28 08:47:11 -0700

    Fix URL for webstatus

--- local_configs/webdashboard.php
+++ local_configs/webdashboard.php
@@ -2,4 +2,4 @@
 
 const DEBUG = false;
 const LANG_CHECKER = 'http://localhost/langchecker/';
-const WEBPROJECTS_JSON = 'https://l10n.mozilla-community.org/~flod/webstatus/web_status.json';
+const WEBPROJECTS_JSON = 'https://l10n.mozilla-community.org/webstatus/web_status.json';
