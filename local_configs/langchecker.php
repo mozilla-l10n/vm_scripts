a419d0cc5d778c79b805169faf67122acb184ef1
Francesco Lodolo [:flod] <flod@lodolo.net>
2017-08-12 15:42:19 -0700

    Update config

--- local_configs/langchecker.php
+++ local_configs/langchecker.php
@@ -4,4 +4,5 @@ $local_storage = '/home/l10n/mozilla/repositories/';
 $locamotion_repo  = '/home/l10n/mozilla/repositories/locamotion/';
 $webroot_folder = '/langchecker/';
 const STORES_L10N = 'http://localhost/stores_l10n/api/v1/';
+const QUERY_L10N = 'https://l10n.mozilla-community.org/mozilla-l10n-query/';
 const DEBUG = false;
